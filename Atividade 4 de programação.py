5dac018b4acd71fe2e6116d247550d1d58a30e46
Kessy Jhony 1º info <112711350+Kessy0875@users.noreply.github.com>
2022-09-21 13:27:40 -0300

    Add files via upload

---
+++ Atividade 4 de programação.py
@@ -0,0 +1,12 @@
+#Data input
+
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+n3 = float(input("enter the third number: "))
+
+#Processing
+
+numbers = [ n1, n2, n3 ]
+numbers.sort()
+
+print(numbers)
